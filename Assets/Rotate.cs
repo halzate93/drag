8e5e41a5adb62702523e81a910daa11906688a92
Harold Satizabal <haroldsati@gmail.com>
2017-08-18 15:59:09 -0500

    Implement camera rotation

---
+++ Assets/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+	void Update ()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(horizontal, vertical);
+
+        transform.Translate(movement);
+        transform.LookAt(target);
+    }
+}
